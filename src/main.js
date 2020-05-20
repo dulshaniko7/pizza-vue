70adba043f2b8cd680eca40ee73c0df8d9a7e7e1
abc <abc@example.com>
2020-05-20 19:58:42 +0530

    'change-demo'

--- src/main.js
+++ src/main.js
@@ -1,5 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
+import 'jquery'
+import 'jquery/dist/jquery'
+import 'popper.js'
+import 'popper.js/dist/popper'
+import 'bootstrap'
+import 'bootstrap/dist/css/bootstrap.css'
 
 Vue.config.productionTip = false
 
